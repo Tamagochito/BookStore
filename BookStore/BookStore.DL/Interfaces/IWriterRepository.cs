483fedab7f1623a5ff822d0b8ac53fc6e2b49a15
Tamagochito <Tamagochito10@abv.bg>
2025-01-29 18:36:54 +0200

    mongo_writer

---
+++ BookStore/BookStore.DL/Interfaces/IWriterRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Models.DTO;
+
+namespace BookStore.DL.Interfaces
+{
+    public interface IWriterRepository
+    {
+        void AddWriter(Writer writer);
+        IEnumerable<Writer> GetWritersByIds(IEnumerable<string> writersIds);
+        Writer? GetById(string id);
+    }
+}
