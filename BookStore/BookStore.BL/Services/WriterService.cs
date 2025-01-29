1cc6ddb5fb35628dcc7e1e9adfc6c0b96a6bbc50
Tamagochito <Tamagochito10@abv.bg>
2025-01-29 18:06:28 +0200

    mongo_writer

---
+++ BookStore/BookStore.BL/Services/WriterService.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.DL.Interfaces;
+using BookStore.Models.DTO;
+
+namespace BookStore.BL.Services
+{
+    internal class WriterService : IWriterService
+    {
+        private readonly IWriterRepository _writerRepository;
+        public WriterService(IWriterRepository writerRepository)
+        {
+            _writerRepository = writerRepository;
+        }
+
+        public void Add(Writer writer)
+        {
+            _actorRepository.AddWriter(writer);
+        }
+    }
+}
