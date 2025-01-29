8762a0d01315c894de096c9af1b9ffa3972b3ff6
Tamagochito <Tamagochito10@abv.bg>
2025-01-29 18:39:46 +0200

    mongo

---
+++ BookStore/BookStore.DL/Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models.DTO;
+
+namespace BookStore.DL.Interfaces
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllBooks();
+        void AddBook(Book book);
+        Book? GetBookById(string id);
+
+        //void UpdateBook(Book book);
+        //void DeleteBook(int id);
+    }
+}
