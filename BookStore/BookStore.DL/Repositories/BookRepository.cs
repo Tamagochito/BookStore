01593e4dfed44ca5199619f484895a58e0894c01
Tamagochito <Tamagochito10@abv.bg>
2025-01-29 19:16:54 +0200

    mongo_writer

---
+++ BookStore/BookStore.DL/Repositories/BookRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.DL.StaticDB;
+using BookStore.Models.DTO;
+
+namespace BookStore.DL.Repositories
+{
+    //[Obsolete]
+    //internal class BookStaticRepository 
+    //{
+    //    public List<Book> GetAllBooks()
+    //    {
+    //        return InMemoryDb.Books;
+    //    }
+
+    //    public void AddBook(Book book)
+    //    {
+    //        if (book == null) return;
+
+    //        book.Id = Guid.NewGuid().ToString();
+    //        InMemoryDb.Books.Add(book);
+    //    }
+
+    //    /// <summary>
+    //    /// Get book by id
+    //    /// </summary>
+    //    /// <param name="id"></param>
+    //    /// <returns></returns>
+    //    public Book? GetBookById(string id)
+    //    {
+    //       return InMemoryDb.Books
+    //           .FirstOrDefault(b => b.Id == id);
+    //    }
+    //}
+}
