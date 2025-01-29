269db17b78ad883b8d61f7c565ec3b5fbb2f3d2c
Tamagochito <Tamagochito10@abv.bg>
2025-01-29 19:08:33 +0200

    mongo_writer

---
+++ BookStore/BookStore.DL/Repositories/ActorstaticRepository.cs
@@ -0,0 +1,34 @@
+﻿using BookStore.DL.Interfaces;
+using BookStore.Models.DTO;
+
+namespace BookStore.DL.Repositories
+{
+    //public class WriterStaticRepository : IWriterRepository
+    //{
+    //    public IEnumerable<Writer> GetWritersByIds(IEnumerable<int> writersIds)
+    //    {
+    //        var result = new List<Writer>();
+
+    //        foreach (var writersId in writersIds)
+    //        {
+    //            foreach (var writer in StaticDB.InMemoryDb.Writers)
+    //            {
+    //                if (writer.Id == writersId)
+    //                {
+    //                    result.Add(writer);
+    //                }
+    //            }
+    //        }
+
+    //        return result;
+
+    //    }
+
+
+    //    public Writer? GetById(int id)
+    //    {
+    //        return StaticDB.InMemoryDb.Writers.
+    //            FirstOrDefault(x => x.Id == id);
+    //    }
+    //}
+}
