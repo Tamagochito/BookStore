c0f3dc18b74b4c0c856ce3637fa73e04aa05d6ef
Tamagochito <Tamagochito10@abv.bg>
2025-01-30 19:40:15 +0200

    mongo_writer

---
+++ BookStore/BookStore.Models/DTO/Writer.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Models.DTO
+{
+    public class Writer
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; } 
+            = string.Empty;
+    }
+}
