74c6c5ed98f1c25b2a87239919a96dde0d56760c
Tamagochito <Tamagochito10@abv.bg>
2025-01-31 19:12:27 +0200

    initial setup
    add DI

---
+++ BookStore/BookStore/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.BL;
+
+namespace BookStore
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.RegisterDataLayer();
+            services.RegisterBusinessLayer();
+        }
+    }
+}
