f74161a0c9ab2f8ff8129f1d08dd1a65004fb216
Tamagochito <Tamagochito10@abv.bg>
2025-06-07 10:50:51 +0300

    Update BookBlServiceTests.cs

--- BookStore/BookService.Tests/BookBlServiceTests.cs
+++ BookStore/BookService.Tests/BookBlServiceTests.cs
@@ -1,39 +1,17 @@
+﻿using Castle.Core.Logging;
+using DnsClient.Internal;
+using Microsoft.Extensions.Logging;
 using Moq;
-using BookStore.BL.Interfaces;
 using BookStore.BL.Services;
 using BookStore.DL.Interfaces;
 using BookStore.Models.DTO;
 
-namespace BookService.Tests
+namespace BookStore.Tests
 {
-    public class BookBlServiceTests
+    public class BookServiceTests
     {
-        private Mock<IBookService> _bookServiceMock;
-        private Mock<IWriterRepository> _writerRepositoryMock;
-
-        public BookBlServiceTests()
-        {
-            _bookServiceMock = new Mock<IBookService>();
-            _writerRepositoryMock = new Mock<IWriterRepository>();
-        }
-
-        private List<Book> _books = new List<Book>
-        {
-            new Book()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Title = "Book 1",
-                Year = 2021,
-                Writers = ["Writer 1", "Writer 2"]
-            },
-            new Book()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Title = "Movie 2",
-                Year = 2022,
-                Writers = ["Writer 3", "Writer 4"]
-            }
-        }; 
+        private readonly Mock<IBookRepository> _bookRepositoryMock;
+        private readonly Mock<IWriterRepository> _writerRepositoryMock;
 
         private List<Writer> _writers = new List<Writer>
         {
@@ -42,49 +20,124 @@ namespace BookService.Tests
                 Id = "157af604-7a4b-4538-b6a9-fed41a41cf3a",
                 Name = "Writer 1"
             },
-            new Writer()
+            new Actor()
             {
                 Id = "baac2b19-bbd2-468d-bd3b-5bd18aba98d7",
                 Name = "Writer 2"
             },
-            new Writer()
+            new Actor()
             {
                 Id = "5c93ba13-e803-49c1-b465-d471607e97b3",
                 Name = "Writer 3"
             },
-            new Writer()
+        };
+
+        private List<Book> _books = new List<Book>()
+        {
+            new Book()
             {
-                Id = "9badefdc-0714-4581-80ae-161cd0a5abbe",
-                Name = "Writer 4"
+                Id = Guid.NewGuid().ToString(),
+                Title = "Book 1",
+                Year = 2021,
+                Writer = [
+                    "157af604-7a4b-4538-b6a9-fed41a41cf3a",
+                    "baac2b19-bbd2-468d-bd3b-5bd18aba98d7"]
+            },
+            new Book()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Title = "Book 2",
+                Year = 2022,
+                Writer = [
+                    "157af604-7a4b-4538-b6a9-fed41a41cf3a",
+                    "5c93ba13-e803-49c1-b465-d471607e97b3"
+                ]
             }
         };
 
-        [Fact]
-        public void GetDetailedBooks_Ok()
+        public BookServiceTests()
         {
-            //setup
-            var expectedCount = 2;
+            _bookRepositoryMock = new Mock<IBookRepository>();
+            _writerRepositoryMock = new Mock<IWriterRepository>();
+        }
 
-            _bookServiceMock
-                .Setup(x => x.GetAllBooks())
-                .Returns(_books);
-            _writerRepositoryMock.Setup(x => 
-                    x.GetById(It.IsAny<string>()))
-                .Returns((string id) =>
-                    _writers.FirstOrDefault(x => x.Id == id));
+        [Fact]
+        void GetById_Ok()
+        {
+            //Arrange
+            var bookId = _books[0].Id;
 
-            //inject
-            var bookBlService = new BookBlService(
-                _bookServiceMock.Object,
-                _writerRepositoryMock.Object);
+            _bookRepositoryMock.Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns((string id) => _books.FirstOrDefault(m => m.Id == id));
+
+            //var logger = Mock.Of<ILogger<BooksService>>();
+            var loggerMock = new Mock<ILogger<BooksService>>();
+            ILogger<BooksService> logger = loggerMock.Object;
 
             //Act
-            var result =
-                bookBlService.GetDetailedBooks();
+            var bookService = new BooksService(
+                _bookRepositoryMock.Object,
+                logger,
+                _writerRepositoryMock.Object);
+
+            var result = bookService.GetById(bookId);
 
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(expectedCount, result.Count);
+            Assert.Equal(bookId, result.Id);
+        }
+
+        [Fact]
+        void GetById_NotExistingId()
+        {
+            //Arrange
+            var bookId = Guid.NewGuid().ToString();
+
+            _bookRepositoryMock.Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns((string id) => _books.FirstOrDefault(m => m.Id == id));
+
+            var loggerMock = new Mock<ILogger<BooksService>>();
+            ILogger<BooksService> logger = loggerMock.Object;
+
+            //Act
+            var bookService = new BooksService(
+                _bookRepositoryMock.Object,
+                logger,
+                _writerRepositoryMock.Object);
+
+            var result = bookService.GetById(bookId);
+
+            //Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        void GetById_WrongGuidId()
+        {
+            //Arrange
+            var bookId = "F";
+
+            _bookRepositoryMock.Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns((string id) => _books.FirstOrDefault(m => m.Id == id));
+
+            //var logger = Mock.Of<ILogger<BooksService>>();
+            var loggerMock = new Mock<ILogger<BooksService>>();
+            ILogger<BookService> logger = loggerMock.Object;
+
+            //Act
+            var bookService = new BookService(
+                _bookRepositoryMock.Object,
+                logger,
+                _writerRepositoryMock.Object);
+
+            var result = bookService.GetById(bookId);
+
+            //Assert
+            Assert.Null(result);
+        }
+    }
+}
+}
         }
 
     }
